f1c65f483183cd4aed3dc4b054cbbf0aa16ea92a
Helen0105 <marinenkoolena21@gmail.com>
2026-05-10 12:22:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const targetDate = new Date("Jul 7, 2026");
+
+const daysEl = document.querySelector('[data-value="days"]');
+const hoursEl = document.querySelector('[data-value="hours"]');
+const minutesEl = document.querySelector('[data-value="mins"]');
+const secondsEl = document.querySelector('[data-value="secs"]');
+
+const id = setInterval(() => {
+    const currentDate = new Date();
+    const time = targetDate - currentDate;
+
+    if (time <= 0) {
+        clearInterval(id);
+        daysEl.textContent = '0';
+        hoursEl.textContent = '00';
+        minutesEl.textContent = '00';
+        secondsEl.textContent = '00';
+        return;
+    }
+
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+
+    daysEl.textContent = days;
+    hoursEl.textContent = String(hours).padStart(2, '0');
+    minutesEl.textContent = String(mins).padStart(2, '0');
+    secondsEl.textContent = String(secs).padStart(2, '0');
+}, 1000);
